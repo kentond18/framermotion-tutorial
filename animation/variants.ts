f2d58e017c40ed43d21782d7fd725167d74ccffd
Kenton Duprey <kentonduprey@icloud.com>
2022-11-27 22:51:37 -0500

    Adding onAnimationComplete handler

--- animation/variants.ts
+++ animation/variants.ts
@@ -27,3 +27,15 @@ export const item: Variants = {
 		},
 	},
 };
+
+export const itemMain: Variants = {
+	hidden: { opacity: 0, y: 200 },
+	show: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			ease: [0.6, 0.01, -0.05, 0.95],
+			duration: 1.6,
+		},
+	},
+};
--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,16 +1,9 @@
 import { motion, Variants } from "framer-motion";
 import React, { useEffect } from "react";
-import { container, item } from "../animation/variants";
+import { container, item, itemMain } from "../animation/variants";
 import Image from "./Image";
 
 const Loader = ({ setLoading }: { setLoading: (val: boolean) => void }) => {
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setLoading(false);
-		}, 4000);
-		return () => clearTimeout(timer);
-	});
-
 	return (
 		<div className="loader">
 			<motion.div
@@ -19,15 +12,16 @@ const Loader = ({ setLoading }: { setLoading: (val: boolean) => void }) => {
 				initial="hidden"
 				animate="show"
 				exit="exit"
+				onAnimationComplete={() => setLoading(false)}
 			>
 				<ImageBlock variants={item} id="image-1" />
-				<div className="transition-image">
+				<motion.div className="transition-image" variants={itemMain}>
 					<Image
 						src={`/images/image-2.jpg`}
 						fallback={"/images/image-2.webp"}
 						alt="random alt"
 					/>
-				</div>
+				</motion.div>
 				<ImageBlock variants={item} id="image-3" />
 				<ImageBlock variants={item} id="image-4" />
 				<ImageBlock variants={item} id="image-5" />
